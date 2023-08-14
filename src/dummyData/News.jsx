fc2a1cfdcae782e821f5ec74df0f957fe7c976c3
PankajSaini9127 <pankajsolanki6008@gmail.com>
2023-08-14 12:52:17 +0530

    Revert " ss"
    This reverts commit 84dc6503068e3c96b604eb19c596129de4842584.

---
+++ src/dummyData/News.jsx
@@ -0,0 +1,28 @@
+
+//img
+import news1 from "../assets/Images/home/epicpaws-blog4.jpg";
+import news2 from "../assets/Images/home/epicpaws-blog6.jpg";
+import news3 from "../assets/Images/home/epicpaws-blog8.jpg";
+
+
+export const news = [
+    {
+      image: news1,
+      title: "Get Paid To Walk Dogs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo a voluptatibus similique molestias, sunt deleniti....",
+    },
+    {
+      image: news2,
+      title: "What Dose A Dog Walker Do ?",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo a voluptatibus similique molestias, sunt deleniti....",
+    },
+    {
+      image: news3,
+      title: "Walk your best doggy friend",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo a voluptatibus similique molestias, sunt deleniti....",
+    },
+  ];
+  
